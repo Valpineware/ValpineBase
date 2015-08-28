42aa2ff830294f8e098e8850da3ed4e6ccae95dc
Dan Watkins <dwatkins@valpineware.com>
2015-08-28 16:20:34 -0500

    Added Assert_True and stuff now allows formatting of user-defined types with the operator+

--- Src/ValpineBase/Test/Assert.h
+++ Src/ValpineBase/Test/Assert.h
@@ -9,7 +9,6 @@
 #include <exception>
 #include <QString>
 
-#include <ValpineBase/Property.h>
 #include <ValpineBase/Test/AssertException.h>
 
 namespace vbase { namespace test
@@ -19,12 +18,34 @@ namespace vbase { namespace test
     public:
         template<typename T>
         static void Eq(const QString &filepath, int lineNumber,
+                       const QString &verbatimActual,
+                       const QString &verbatimExpected,
                        const T &actual, const T &expected)
         {
             if (actual != expected)
             {
                 AssertException e;
-                e.pMessage().append(QString("Compared ") + typeid(T).name() + " are not equal");
+                e.pMessage().append(verbatimActual + " != " + verbatimExpected);
+                e.pMessage().append(QString("Expected: ") + expected);
+                e.pMessage().append(QString("Actual: ") + actual);
+
+                e.pFilepath = filepath;
+                e.pLineNumber = lineNumber;
+
+
+                throw e;
+            }
+        }
+
+
+        template<typename T>
+        static void True(const QString &filepath, int lineNumber,
+                         const QString &verbatim, const T &what)
+        {
+            if (!what)
+            {
+                AssertException e;
+                e.pMessage().append("");
                 e.pFilepath = filepath;
                 e.pLineNumber = lineNumber;
 
@@ -33,9 +54,15 @@ namespace vbase { namespace test
             }
         }
     };
-
-#define Assert_Eq(actual, expected) ::vbase::test::Assert::Eq(QString(__FILE__), __LINE__, actual, expected)
-
 }}
+
+#define Assert_Eq(actual, expected) \
+    ::vbase::test::Assert::Eq(QString(__FILE__), __LINE__, \
+                                QString(#actual), QString(#expected), \
+                                actual, expected)
+
+#define Assert_True(what) \
+    ::vbase::test::Assert::True(QString(__FILE__), __LINE__, \
+                                    QString(#what), what)
 
 #endif
