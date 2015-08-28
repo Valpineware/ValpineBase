22a021285f770e144692ee9ebe97cc8c7e8397e7
Dan Watkins <dwatkins@valpineware.com>
2015-08-27 21:22:21 -0500

    Test system now runs each test method on a separate instance of the test class.

--- Src/ValpineBase/Test/Suite.cpp
+++ Src/ValpineBase/Test/Suite.cpp
@@ -1,33 +1,14 @@
-#include <QtCore/QDebug>
-#include <QtCore/QMetaMethod>
-
 #include "Suite.h"
 
 namespace vbase { namespace test
 {
-    QList<QObject*> Suite::mRegistered;
+    QList<Suite::TestClassPackageInterface*> Suite::mRegistered;
 
     void Suite::run()
     {
-        qDebug() << "Discovering tests...";
-
-        for (QObject *test : mRegistered)
+        for (TestClassPackageInterface *test : mRegistered)
         {
-            const auto *metaObject = test->metaObject();
-            qDebug() << "TestClass " << metaObject->className();
-
-
-            QList<QMetaMethod> testMethods;
-
-            for (int i=0; i<metaObject->methodCount(); i++)
-            {
-                auto method = metaObject->method(i);
-                if (QString(method.tag()) == "vTEST")
-                {
-                    testMethods.append(method);
-                    qDebug() << "  Case: " << method.name();
-                }
-            }
+            test->runTests();
         }
     }
 }}
--- Src/ValpineBase/Test/Suite.h
+++ Src/ValpineBase/Test/Suite.h
@@ -5,6 +5,10 @@
 #include <QtCore/QDebug>
 #include <QtCore/QString>
 #include <QtCore/QList>
+#include <QtCore/QMetaObject>
+#include <QtCore/QMetaMethod>
+
+#include <memory>
 
 namespace vbase { namespace test
 {
@@ -18,17 +22,56 @@ namespace vbase { namespace test
         {
             TestAdder()
             {
-                Suite::mRegistered.append(new T);
+                mRegistered.append(new TestClassPackage<T>);
             }
         };
 
     private:
-        static QList<QObject*> mRegistered;
+        class TestClassPackageInterface
+        {
+        public:
+            virtual void runTests() = 0;
+        };
+
+
+        // TODO convert to private implementation
+        template<typename T>
+        class TestClassPackage : public TestClassPackageInterface
+        {
+        public:
+            virtual void runTests() override
+            {
+                const QMetaObject *metaObject = T().metaObject();
+
+                for (int i=0; i<metaObject->methodCount(); i++)
+                {
+                    auto method = metaObject->method(i);
+
+                    if (QString(method.tag()) == "VTEST")
+                    {
+                        T testObject;
+
+                        try
+                        {
+                            method.invoke(&testObject, Qt::DirectConnection);
+                        }
+                        catch (std::exception &e)
+                        {
+                            qWarning() << metaObject->className() << "::"
+                                       << method.name() << "() failure.";
+                        }
+                    }
+                }
+            }
+        };
+
+
+        static QList<TestClassPackageInterface*> mRegistered;
     };
 
 #ifndef Q_MOC_RUN
 // define the tag text
-#  define vTEST
+#  define VTEST
 #endif
 
 #define ADD_TESTCLASS(name) \
